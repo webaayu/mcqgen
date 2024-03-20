efe45fd7ca5e58a38b73ec687488d4b5823773ca
Pratiksha Patel <99045373+webaayu@users.noreply.github.com>
2024-03-20 18:50:50 +0530

    Update MCQGenerator.py

--- src/mcqgenerator/MCQGenerator.py
+++ src/mcqgenerator/MCQGenerator.py
@@ -5,14 +5,15 @@ import traceback
 from dotenv import load_dotenv
 from src.mcqgenerator.utils import read_data_from_url, get_table_data #read_file
 from src.mcqgenerator.logger import logging
-
 from langchain.chat_models import ChatOpenAI
-from langchain.llms import OpenAI #
+from langchain.llms import OpenAI # for openAI LLM
+#from langchain_community.llms import Ollama # To use ollama's llm
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 from langchain.chains import SequentialChain
-from langchain.callbacks import get_openai_callback #
+from langchain.callbacks import get_openai_callback # callback from openai
 import PyPDF2
+
 
 #load environment var from .env
 load_dotenv()
@@ -20,7 +21,7 @@ load_dotenv()
 KEY=os.getenv("OPENAI_API_KEY")
 
 llm = ChatOpenAI(openai_api_key=KEY,model_name="gpt-3.5-turbo", temperature=0.5)
-
+#llm = Ollama(model="mistral") #if you want to use ollama's mistral model
 TEMPLATE="""
 Text:{text}
 You are an expert MCQ maker. Given the above text, it is your job to \
